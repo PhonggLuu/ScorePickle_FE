02a1b81be757dfa942c8b7b3b347c573608f4628
phonggluu <luuphong016@gmail.com>
2025-04-25 00:59:21 +0700

    feat: config realtime

--- src/pages/matchPage/MatchDetail.tsx
+++ src/pages/matchPage/MatchDetail.tsx
@@ -199,6 +199,7 @@ import { useJoinMatch } from '@src/modules/Match/hooks/useJoinMatch';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@src/redux/store';
+import { useMatchRealtimeLogs } from '@src/modules/Match/hooks/useMatchRealtimeLogs';
 
 export default function MatchDetails() {
   const { id } = useParams<{ id: string }>();
@@ -206,6 +207,12 @@ export default function MatchDetails() {
   const { mutate: joinMatch } = useJoinMatch();
   const [isModalVisible, setIsModalVisible] = useState(false);
   const user = useSelector((state: RootState) => state.auth.user);
+  const { logs } = useMatchRealtimeLogs(Number(id), 1);
+  const team1 = logs.filter((log) => log.team === 1);
+  const team2 = logs.filter((log) => log.team === 2);
+  console.log(logs);
+  console.log(team1);
+  console.log(team2);
 
   if (data === undefined) {
     return;
@@ -542,6 +549,30 @@ export default function MatchDetails() {
               </div>
             </div>
           </div>
+          {logs1 && (
+            <div className="d-md-none mt-3">
+              <div
+                className="alert alert-danger d-flex align-items-center"
+                role="alert"
+              >
+                <div className="d-flex align-items-center">
+                  <div className="me-2">
+                    <div
+                      className="spinner-grow spinner-grow-sm text-danger"
+                      role="status"
+                    >
+                      <span className="visually-hidden"></span>
+                    </div>
+                  </div>
+                  <div>
+                    {logs1.map((log, index) => (
+                      <div key={index}>{log.points}</div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
         <Modal
           title="Join Match"
